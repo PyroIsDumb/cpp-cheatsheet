be71342812b9854f0fa2de1c93f4cfb998a19ca3
Alexia <76690969+PyroIsDumb@users.noreply.github.com>
2024-03-23 23:30:36 +0100

    mreowmeeowwwmeowmeownyaameoww
    :3

---
+++ arithmetic-operators.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main (){
+//Arithmatic operators return the result of mathematical operations. (+ - * /)
+
+int students = 20;
+
+//you could write: "students = students + 20;". However, theres a shorter way to do this.
+students+=20;
+
+//If you would only want to add one, you could also use:
+//students++;
+//You will probably see this a lot in loops.
+
+//This also applies subtractions, multiplications and divisions.
+
+
+std::cout << students;
+
+return 0;
+}
